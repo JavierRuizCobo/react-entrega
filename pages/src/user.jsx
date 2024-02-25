284b192c117d63f84a4ef0230259b89e0c228074
JavierRuizCobo <javier.ruiz1@alumnos.uneatlantico.es>
2024-02-25 17:57:28 +0100

    Mejora diseño paginas diseño cada rol

--- pages/src/user.jsx
+++ pages/src/user.jsx
@@ -1,67 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './styles/userStyle.css';
 
 const UserPage = () => {
-  useEffect(() => {
-    const cerrarSesionHandler = () => {
-      localStorage.removeItem('access_token');
-      window.history.replaceState(null, null, '/');
-      window.location.href = '/';
-    };
+  const [userData, setUserData] = useState([]);
 
-    document.getElementById('cerrarSesionBtn').addEventListener('click', cerrarSesionHandler);
-
-    return () => {
-      document.getElementById('cerrarSesionBtn').removeEventListener('click', cerrarSesionHandler);
-    };
-  }, []);
-
-  const llenarTabla = (data) => {
-    const tablaBody = document.getElementById('tablaBody');
-
-    tablaBody.innerHTML = '';
-
-    data.forEach((user) => {
-      const fila = document.createElement('tr');
-      fila.innerHTML = `
-        <td>${user.username}</td>
-        <td>${user.email}</td>
-        <td>${user.roles[0]}</td>
-      `;
-      tablaBody.appendChild(fila);
-    });
-  };
-
-  const obtenerJsonServer = () => {
-    const accessToken = localStorage.getItem('access_token');
-
-    fetch("http://localhost:8080/api/test/user", {
-      method: "GET",
-      headers: {
-        "x-access-token": `${accessToken}`
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Respuesta del servidor para la ruta de usuario:", data);
-      if (data) {
-        llenarTabla(data);
-      }
-    })
-    .catch(error => {
-      console.error("Error al realizar la solicitud GET:", error);
-      window.location.href = '/';
-      localStorage.removeItem('access_token');
-    });
+  const cerrarSesionHandler = () => {
+    localStorage.removeItem('access_token');
+    window.history.replaceState(null, null, '/');
+    window.location.href = '/';
   };
 
   useEffect(() => {
+    const obtenerJsonServer = async () => {
+      try {
+        const accessToken = localStorage.getItem('access_token');
+        const response = await fetch("http://localhost:8080/api/test/user", {
+          method: "GET",
+          headers: {
+            "x-access-token": accessToken
+          }
+        });
+        const data = await response.json();
+        console.log("Respuesta del servidor para la ruta de usuario:", data);
+        if (data) {
+          setUserData(data);
+        }
+      } catch (error) {
+        console.error("Error al realizar la solicitud GET:", error);
+        window.location.href = '/';
+        localStorage.removeItem('access_token');
+      }
+    };
+
     obtenerJsonServer();
   }, []);
 
   return (
     <div className="main">
-      <button id="cerrarSesionBtn">Cerrar Sesión</button>
+      <button onClick={cerrarSesionHandler}>Cerrar Sesión</button>
 
       <h1>Contenido del Usuario</h1>
       <h2>Muestra todos los usuarios con rol user</h2>
@@ -74,7 +50,14 @@ const UserPage = () => {
             <th>Rol</th>
           </tr>
         </thead>
-        <tbody id="tablaBody">
+        <tbody>
+          {userData.map((user) => (
+            <tr key={user.username}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.roles[0]}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
